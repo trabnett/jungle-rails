ccd3ffeb265bb473e0e5ce72d756f7466c783875
trabnett <timrabnett@gmail.com>
2019-02-22 00:02:35 +0000

    added user verification using bcrypt

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
 
-   
+    def show_logged_in_user
+        if current_user
+            "<span> Welcome: #{current_user.first_name} </span>"
+        else
+            ""
+        end
+    end
 end
